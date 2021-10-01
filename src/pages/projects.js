4fae79b513710ce16bc34a4f0095f74755441e03
Andrick <andrickposo3001@gmail.com>
2021-10-01 16:12:04 +0100

    fixes

---
+++ src/pages/projects.js
@@ -0,0 +1,18 @@
+const Projects = () => {
+  return (
+    <div style={{width:"100vw", backgroundColor:"#EBE8E8"}}>
+      <div class="projects">
+        <div class="main-projects">
+          <div class="title">MAIN PROJECTS</div>
+          <div>COMING SOON...</div>
+        </div>
+        <div class="mini-projects">
+          <div class="title">MAIN PROJECTS</div>
+          <div>COMING SOON...</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
